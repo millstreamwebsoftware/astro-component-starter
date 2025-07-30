00fb51cb909f2b689524b5f863da0a34541386c2
Mike Neumegen <mneumegen@gmail.com>
2025-07-30 13:11:16 -0700

    fix: css build issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import bookshop from "@bookshop/astro-bookshop";
+import postcssGlobalData from "@csstools/postcss-global-data";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import autoprefixer from "autoprefixer";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import postcssCustomMedia from "postcss-custom-media";
+import postcssEach from "postcss-each";
+import postcssImport from "postcss-import";
+import postcssNested from "postcss-nested";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -26,6 +32,18 @@ export default defineConfig({
   vite: {
     css: {
       devSourcemap: true,
+      postcss: {
+        plugins: [
+          postcssImport,
+          postcssGlobalData({
+            files: [path.resolve(__dirname, "./src/styles/skele/variables/_media.pcss")],
+          }),
+          postcssCustomMedia,
+          postcssNested,
+          postcssEach,
+          autoprefixer,
+        ],
+      },
     },
     resolve: {
       alias: {
