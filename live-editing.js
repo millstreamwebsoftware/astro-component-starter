43e16afbd64ec35a1188c96f2c7f7cf645f9f025
Mike Neumegen <mneumegen@gmail.com>
2025-12-10 18:42:15 -0800

    fix: list not rendering

--- live-editing.js
+++ live-editing.js
@@ -1,7 +1,6 @@
 import { registerAstroComponent } from "@cloudcannon/editable-regions/astro";
 import "@cloudcannon/editable-regions/astro-react-renderer";
 
-// Dynamically import all components from the components directory
 const componentModules = import.meta.glob("./src/components/**/*.astro", { eager: true });
 
 for (const [path, module] of Object.entries(componentModules)) {
